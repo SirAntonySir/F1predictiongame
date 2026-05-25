e1a9d707f61e93436a58208af5e65afcde73a7e0
SirAntonySir <rockenstein.anton@gmail.com>
2026-05-25 12:57:02 +0200

    chore: clean slate + add redesign deps (go_router, fonts, prefs, intl)

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,17 @@
 import 'package:flutter/material.dart';
-import 'Screens/live_data_screen.dart';
-import 'Screens/live_table_screen.dart';
-import 'Screens/prediction_input_screen.dart';
 
 void main() {
-  runApp(MyTippingGameApp());
+  runApp(const F1PgApp());
 }
 
-class MyTippingGameApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tipping Game',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  // Pages for each tab
-  final List<Widget> _pages = [
-    PredictionInputScreen(),
-    LiveTableScreen(),
-    LiveDataPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class F1PgApp extends StatelessWidget {
+  const F1PgApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Tipping'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.leaderboard), label: 'Leaderboard'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.live_tv), label: 'Live Data'),
-        ],
-      ),
+    return const MaterialApp(
+      title: 'F1PG',
+      home: Scaffold(body: Center(child: Text('F1PG'))),
     );
   }
 }
